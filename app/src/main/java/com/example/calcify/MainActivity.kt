19ae06966ff97f9a5e21b68ba7e0a127beb26983
EstherNyumu <emutheu1998@gmail.com>
2024-04-20 09:30:02 +0300

    First commit

---
+++ app/src/main/java/com/example/calcify/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.calcify
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.calcify.ui.theme.CalcifyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalcifyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Calculator()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Calculator() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(30.dp))
+        Text(
+            text = "CALCIFY",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        intArrayOf()
+        var numbers by rememberSaveable { mutableStateOf("") }
+//        var answer by remember { mutableStateOf("") }
+        TextField(
+            value = numbers,
+            onValueChange = { numbers = it },
+            modifier = Modifier.height(150.dp),
+            readOnly = true
+        )
+//        TextField(value = answer,
+//            onValueChange = {answer= it},
+//            modifier = Modifier.height(150.dp))
+        Spacer(modifier = Modifier.height(30.dp))
+
+        val buttonRows = listOf(
+            listOf("Clear", "Delete"),
+            listOf("9", "8", "7", "+"),
+            listOf("6", "5", "4", "-"),
+            listOf("3", "2", "1", "x"),
+            listOf("/", "0", "=")
+        )
+        buttonRows.forEach { rowItems ->
+            Row {
+                rowItems.forEach { item ->
+                    Button(onClick = {
+                            when (item) {
+                                "Clear" -> numbers = " "
+                                "Delete" -> if (numbers.isNotEmpty())numbers = numbers.dropLast(1)
+                                "=" -> numbers = calculate(numbers)
+                                else -> numbers += item
+                            }
+                    }
+                    ) {
+                        Text(text = item)
+                    }
+                    Spacer(modifier = Modifier.width(10.dp))
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+
+
+
+
+
+
+    }
+}
+
+fun calculate(numbers: String): String {
+
+    var myList = (numbers.split("+"))
+    val a = myList.get(0);
+    val b = myList.get(1)
+    var result  = a.toInt() + b.toInt()
+
+    return result.toString()
+}
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        CalcifyTheme {
+            Calculator()
+        }
+    }
